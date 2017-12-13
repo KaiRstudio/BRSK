8f34c248f206f3e09f289950a9e9f001d1f6197c
KaiRstudio <playdesseb.de>
2017-12-13 15:50:40 +0100

    fghfghj

--- BADSproj.R
+++ BADSproj.R
@@ -1,22 +1,22 @@
+# ---- INTRO ----
 # GROUP 45 members:
-#   - Bastian
+#   - Sebastia
 #   - Rieke
-#   - Sena
+#   - Sena Aydin (594644) - senaaydin484@gmail.com
 #   - Kai Dessau (559766) - kai-dessau@web.de
-setwd("~/Humboldt/BADS/BRSK")
 
-daten <- read.csv('~/Humboldt/BADS/BRSK',
-                    sep=",",
-                    na.strings = c("?","not reported"),
-                    colClasses = c("order_date" ="Date",
-                                   "delivery_date" = "Date",
-                                   "user_reg_date" = "Date",
-                                   "user_dob" = "Date",
-                                   "order_item_id" = "factor",
-                                   "item_id" = "factor",
-                                   "brand_id" = "factor" ,
-                                   "user_id" = "factor",
-                                   "return" = "factor"))
+daten <- read.csv('C:\\Users\\Kai\\Desktop\\BADS_assignment',
+                  sep=",",
+                  na.strings = c("?","not reported"),
+                  colClasses = c("order_date" ="Date",
+                                 "delivery_date" = "Date",
+                                 "user_reg_date" = "Date",
+                                 "user_dob" = "Date",
+                                 "order_item_id" = "factor",
+                                 "item_id" = "factor",
+                                 "brand_id" = "factor" ,
+                                 "user_id" = "factor",
+                                 "return" = "factor"))
 
 # ---- Packages ----
 if(!require("plyr"))          install.packages("plyr");         library("plyr")
@@ -34,26 +34,23 @@ if(!require("rpart.plot"))    install.packages("rpart.plot");   library("rpart.p
 if(!require("RColorBrewer"))  install.packages("RColorBrewer"); library("RColorBrewer")
 if(!require("rms"))           install.packages("rms");          library("rms")
 if(!require("pROC"))          install.packages("pROC");         library("pROC")
+if(!require("e1071"))         install.packages("e1071");        library("e1071")
+if(!require("randomforest"))  install.packages("randomforest"); library("randomforest")
+if(!require("hmeasure"))      install.packages("hmeasure");     library("hmeasure")
 # review at the end which ones we really need
-
-
 
 # ---- General Terms ----
 formatofdate <- "%Y-%m-%d"
 
-age = function(from, to) {
+age <- function(from, to) {
   from_lt = as.POSIXlt(from)
   to_lt = as.POSIXlt(to)
-  
   age = to_lt$year - from_lt$year
-  
   ifelse(
     to_lt$mon < from_lt$mon |
       (to_lt$mon == from_lt$mon & to_lt$mday < from_lt$mday),
     age - 1,
-    age
-  )
-} #-------I used this function to calculate age so maybe we can use it later on. Therefore I leave it here.
+    age)} #-------I used this function to calculate age so maybe we can use it later on. Therefore I leave it here.
 
 Z.outlier <- function(x){
   Zscore <- scale(x)
@@ -62,9 +59,8 @@ Z.outlier <- function(x){
   return(x)    
 }
 
-#######Converting dates
 for (chrVar in c("order_date", "delivery_date", "user_reg_date", "user_dob")) {
- daten[, chrVar] <- as.Date(daten[[chrVar]])
+  daten[, chrVar] <- as.Date(daten[[chrVar]])
 }
 
 
@@ -72,59 +68,94 @@ for (chrVar in c("order_date", "delivery_date", "user_reg_date", "user_dob")) {
 table(is.na(daten$order_item_id))
 
 # ---- Dates ----
-x$delivery_date[x$order_date>x$delivery_date] <-NA
+daten$delivery_date[daten$order_date>daten$delivery_date] <-NA
 daten$delivery_date[daten$delivery_date == "1990-12-31"] <- NA # remove unrealistic times
+daten$delivery_time <- daten$delivery_date - daten$order_date #Subtract delivery date from order date so we can see "delivery time"
+daten$order_month <- as.factor(months(daten$order_date)) # new column with month of delivery
+daten$delivery_time <- Z.outlier(daten$delivery_time) # removing outliers from delivery time
+MED_delivery <- round( median (daten$delivery_time, na.rm =TRUE)) # round( mean (daten$delivery_time, na.rm =TRUE)) gives median of 4 and mean of 11 for delivery time
+daten$delivery_date[is.na(daten$delivery_date)]<- daten$order_date[is.na(daten$delivery_date)] + MED_DEL
+daten$delivery_time[is.na(daten$delivery_time)] <- MED_DEL
+daten$regorderdiff <- daten$order_date - daten$user_reg_date
+
+
+
+hist(as.numeric(daten$regorderdiff)) # many people that registered and immediately bought, the rest is equally distributed up until 774 days
+max(daten$delivery_time, na.rm =TRUE) # delivery has to be timely after order date, the max is 151 days
+min(daten$delivery_time, na.rm =TRUE) # check that minimal delivery time is zero
+hist(daten$delivery_time) # Histogram of delivery_time
 sum(is.na(daten$delivery_date)) # should be zero
-daten$delivery.time<-daten$delivery_date - daten$order_date #Subtract delivery date from order date so we can see "delivery time" which might be important for prediction.
-x$order_month <- as.factor(months(x$order_date))
-
-
-order$delivery.time<-order$delivery_date - order$order_date
-order$delivery.time_factor[is.na(order$delivery.time)]<-"neverReturned"
-order$delivery.time_factor[order$delivery.time<0]<-"rarelyReturned"
-order$delivery.time_factor[order$delivery.time>=0]<-"returnedHalfTheTime"
-order$delivery.time_factor<- as.factor(order$delivery.time_factor)
 # ---- Item ID ----
 
 # ---- Size ----
-x$item_size <- factor(toupper(x$item_size))
-table_size <- table(x$item_size)
-levels(x$item_size) <- c(names(table_size), "Other")
-x$item_size[is.na(x$item_size)] <- factor("Other")
-x[x$item_size %in% names(table_size)[table_size < 100],"item_size"] <- factor("Other")
-x$item_size <- factor(x$item_size)
+daten$item_size <- factor(toupper(daten$item_size))
+table_size <- table(daten$item_size)
+levels(daten$item_size) <- c(names(table_size), "Other")
+daten$item_size[is.na(x$item_size)] <- factor("Other")
+daten[daten$item_size %in% names(table_size)[table_size < 100],"item_size"] <- factor("Other")
+daten$item_size <- factor(daten$item_size)
 # ---- Color ----
-table_col_clust <- table(x$item_color)
-levels(x$item_color) <- c(names(table_col_clust), "Other")
-x$item_color[is.na(x$item_color)] <- factor("Other")
-x[x$item_color %in% names(table_col_clust)[table_col_clust < 200],"item_color"] <- factor("Other")
-x$item_color <- factor(x$item_color)
+table_col_clust <- table(daten$item_color)
+levels(daten$item_color) <- c(names(table_col_clust), "Other")
+daten$item_color[is.na(daten$item_color)] <- factor("Other")
+daten[daten$item_color %in% names(table_col_clust)[table_col_clust < 200],"item_color"] <- factor("Other")
+daten$item_color <- factor(daten$item_color)
 
-
+daten$item_colorcat <- as.factor(daten$item_color) # categorisation is very subjective
+daten$item_colorcat[daten$item_colorcat == "?"] <- NA
+daten$item_colorcat <- ifelse(daten$item_colorcat %in% c("?"), NA,
+                              ifelse(daten$item_colorcat %in% c("petrol", "blau", "blue", "azure", "cobalt blue", "dark navy", "darkblue", "turquoise", "silver", "navy", "aqua", "aquamarine", "baltic blue", "darkblue"), "blue",
+                                     ifelse(daten$item_colorcat %in% c("red", "orange", "purple", "currant purple", "pink", "antique pink", "crimson", "bordeaux", "berry", "fuchsia", "perlmutt", "coral", "hibiscus", "magenta", "terracotta", "dark garnet"), "red",
+                                            ifelse(daten$item_colorcat %in% c("green", "olive", "oliv", "dark oliv", "mint", "aubergine", "lemon", "nature", "khaki", "avocado", "jade"), "green",
+                                                   ifelse(daten$item_colorcat %in% c("mocca", "brwon", "brown", "beige", "kanel", "mahagoni", "copper coin", "cognac", "caramel"),"brown",
+                                                          ifelse(daten$item_colorcat %in% c("apricot", "yellow", "vanille", "gold", "ingwer", "white", "mango", "almond", "champagner", "creme", "curry", "ivory", "ocher"), "yellow",
+                                                                 ifelse(daten$item_colorcat %in% c("grey", "black", "dark grey", "graphite", "iron", "habana", "ebony", "amethyst", "basalt", "ash", "ancient", "anthracite", "denim", "dark denim"), "dark",
+                                                                        ifelse(daten$item_colorcat %in% c("striped", "aviator", "pallid", "opal", "stained", "floral", "ecru", "curled"), "others", daten$item_colorcat)))))))
+table(daten$item_colorcat) 
 # ---- Brand ID ----
 
 # ---- Price ----
-x$item_price[x$item_price==0] <-NA
+daten$pricecat <- daten$item_price
+daten$pricecat <- cut(daten$pricecat,c(-1, 0, 20, 50, 100, 200, 400), labels=c(NA, "0-20", "20-50", "50-100", "100-200", "200-400"))
 
-med.ip <- median(x$item_price, na.rm=TRUE)
-x$item_price[is.na(x$item_price)] <- med.ip
+daten$item_price[daten$item_price == 0] <-NA
+med_item_price <- median(daten$item_price, na.rm=TRUE)
+daten$item_price[is.na(daten$item_price)] <- med_item_price
+daten$item_price <- Z.outlier(daten$item_price)
 # ---- User ID ----
 
 # ---- Title ----
 daten$user_title[daten$user_title == "not reported"] <- NA # remove not reported titles
 table(daten$user_title)
 
-table_title <- table(x$user_title)
-levels(x$user_title) <- c(names(table_title), "Other")
-x$user_title[is.na(x$user_title)] <- factor("Other")
-x$user_title[x$user_title == "Company"] <- factor("Other")
-x$user_title[x$user_title == "Family"] <- factor("Other")
-x$user_title <- factor(x$user_title)
+table_title <- table(daten$user_title)
+levels(daten$user_title) <- c(names(table_title), "Other")
+daten$user_title[is.na(daten$user_title)] <- factor("Other")
+daten$user_title[daten$user_title == "Company"] <- factor("Other")
+daten$user_title[daten$user_title == "Family"] <- factor("Other")
+daten$user_title <- factor(daten$user_title)
 # ---- Age ----
+daten$age <- as.numeric(round((daten$order_date - daten$user_dob)/ 365.25))
+
 med.age <- median(x$age, na.rm = TRUE)
 x$age[is.na(x$age)] <- med.age
+Returns$age <- Z.outlier(Returns$age) 
+
+daten$age2 <- daten$age
+daten$age2[is.na(daten$age2)] <- round(median(daten$age2, na.rm = T))
+daten$age2 <- ageoutlierclean(daten$age2)
+
+daten$agecat <- recode(daten$agecat, "0:27='18-27';28:37='28-37'; 38:47='38-47'; 48:57='48-57'; 58:67='58-67'; 68:77='68-77'; 78:87='78-87'; 88:100='88-100'")
+
+table(daten$agecat)
+summary(daten$age2)
+hist(daten$age2)
+boxplot(daten$age2)
+hist(daten$age)
 # ---- User State ----
 
+
+missmap(daten, main = "Missing values vs observed") # to give a plot of the missing values per variable
 
 # ---- IDEAS ----
 # 1)  In case someone buys more stuff at once, the whole thing should obviously not be 
