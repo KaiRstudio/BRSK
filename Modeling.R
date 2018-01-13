0f9d677cc30abdea15375a08fcf01c37b96154c6
KaiRstudio <playdesseb.de>
2018-01-13 13:30:38 +0100

    arch2

--- Modeling.R
+++ Modeling.R
@@ -1,1 +1,15 @@
 # file for building models
+
+# ---- Random Forest ----
+
+# ---- Gradient Boosting ----
+
+# ---- Neural Networks ----
+
+# ---- Logistic Regression ----
+
+# For each model:
+# Parameter optimization (=Loops over parameters of model)
+# Loops over choice of variables
+# Training/Testing    
+# data splitting, model prediction, cross validation
--- toptier.R
+++ toptier.R
@@ -1,3 +1,7 @@
 source("https://raw.githubusercontent.com/KaiRstudio/BRSK/master/BADSproj.R") # loads and cleans data
 
-source("")
+source("https://raw.githubusercontent.com/KaiRstudio/BRSK/master/VarSel.R")   # uses VARIABLE SELECTION code
+
+source("https://raw.githubusercontent.com/KaiRstudio/BRSK/master/AdapToCSL.R")# uses ADAPTATION TO COST SENSITIVE LEARNING code
+
+source("https://raw.githubusercontent.com/KaiRstudio/BRSK/master/Modeling.R") # uses MODELING code
