4d311ad781ca532cff9dca22ee6af4b40af10555
BanjoBasti <s.guetgemann@gmx.net>
2018-01-20 13:44:49 +0100

    Correction of data prep so far - introducing clean structure

--- BADSproj.R
+++ BADSproj.R
@@ -1,9 +1,13 @@
+#   BADS Final Assignment Group 
 #   - Sebastian Gütgemann (589623)  ~ s.guetgemann@gmx.net
 #   - Rieke Ackermann     (588611)  ~ rieke.ackermann@web.de
 #   - Sena Aydin          (594644)  ~ senaaydin484@gmail.com
 #   - Kai Dessau          (559766)  ~ kai-dessau@web.de
 
-# ---- Packages ----
+
+
+# ----------------------- Packages
+
 if(!require("plyr"))          install.packages("plyr");         library("plyr")
 if(!require("dplyr"))         install.packages("dplyr");        library("dplyr")
 if(!require("stringdist"))    install.packages("stringdist");   library("stringdist")
@@ -29,10 +33,20 @@ if(!require("xgboost"))       install.packages("xgboost");      library("xgboost
 if(!require("penalized"))     install.packages("penalized");    library("penalized")
 if(!require("neuralnet"))     install.packages("neuralnet");    library("neuralnet")
 
-# ---- Load Data ----
+
+
+
+# ----------------------- Load Data
+
 githubURL <- "https://raw.githubusercontent.com/KaiRstudio/BRSK/master/BADS_WS1718_known_MODEL_FITTING.csv"
 daten <- source_data(githubURL, sha1 ="254e37cf5b7fe121e2e9c8212803cda9415c9de7", header = "auto", sep=",")
 #sha1 is the hash to make sure data hasnt changed
+
+
+
+
+# ----------------------- Formatting
+
 formatofdate        <- "%Y-%m-%d"
 daten$order_date    <- as.Date(daten$order_date, format = formatofdate)
 daten$delivery_date <- as.Date(daten$delivery_date, format = formatofdate)
@@ -46,7 +60,12 @@ daten$user_title    <- as.factor(daten$user_title)
 daten$user_state    <- as.factor(daten$user_state)
 daten$return        <- as.factor(daten$return)
 
-# ---- General Terms ----
+
+
+
+# ----------------------- General Terms
+
+# - Calculate age -
 age <- function(from, to) {
   from_lt = as.POSIXlt(from)
   to_lt = as.POSIXlt(to)
@@ -57,6 +76,8 @@ age <- function(from, to) {
     age - 1,
     age)} 
 
+
+# - Exclude Outlier by Z-Score -
 Z.outlier <- function(x){
   Zscore <- scale(x)
   x[Zscore >3] <- NA
@@ -64,24 +85,28 @@ Z.outlier <- function(x){
   return(x)}
 
 
-# ---- Dates & TIMES ----
+# - Aggregate color levels -
+agg.col <- function (df.col) {
+  df.col <- factor(df.col)
+  df.col <- ifelse(df.col %in% c("?"), NA,
+                             ifelse(df.col %in% c("petrol", "blau", "blue", "azure", "cobalt blue", "dark navy", "darkblue", "turquoise", "silver", "navy", "aqua", "aquamarine", "baltic blue", "darkblue"), "blue",
+                                    ifelse(df.col %in% c("red", "orange", "purple", "currant purple", "pink", "antique pink", "crimson", "bordeaux", "berry", "fuchsia", "perlmutt", "coral", "hibiscus", "magenta", "terracotta", "dark garnet"), "red",
+                                           ifelse(df.col %in% c("green", "olive", "oliv", "dark oliv", "mint", "aubergine", "lemon", "nature", "khaki", "avocado", "jade"), "green",
+                                                  ifelse(df.col %in% c("mocca", "brwon", "brown", "beige", "kanel", "mahagoni", "copper coin", "cognac", "caramel"),"brown",
+                                                         ifelse(df.col %in% c("apricot", "yellow", "vanille", "gold", "ingwer", "white", "mango", "almond", "champagner", "creme", "curry", "ivory", "ocher"), "yellow",
+                                                                ifelse(df.col %in% c("grey", "black", "dark grey", "graphite", "iron", "habana", "ebony", "amethyst", "basalt", "ash", "ancient", "anthracite", "denim", "dark denim"), "dark", "Other")))))))
+  df.col <- factor(df.col)
+}
 
-# Clean Date
-daten$delivery_date[daten$order_date>daten$delivery_date] <-NA # remove unrealistic times "automatically"
 
 
-# Create new Date dependend variables (cleaned)
-daten$order_month <- as.factor(months(daten$order_date)) # new column with month of delivery
-#MED_DEL <- round( median (daten$delivery_time, na.rm =TRUE)) # round( mean (daten$delivery_time, na.rm =TRUE)) gives median of 4 and mean of 11 for delivery time
-#daten$delivery_date[is.na(daten$delivery_date)]<- daten$order_date[is.na(daten$delivery_date)] + MED_DEL
-#daten$delivery_time[is.na(daten$delivery_time)] <- MED_DEL
-daten$delivery_time <- Z.outlier(as.numeric(daten$delivery_date - daten$order_date))
-daten$regorderdiff <- as.numeric(daten$order_date - daten$user_reg_date) ## makes sense?
+
+# ----------------------- Prep Existing Variables
 
 
-# Scaled variables
-daten$delivery_time2 <- as.numeric(scale(daten$delivery_time))
-daten$regorderdiff2 <- as.numeric(scale(daten$regorderdiff))
+
+# ---- Dates ----
+daten$delivery_date[daten$order_date>daten$delivery_date] <-NA
 
 
 
@@ -89,80 +114,94 @@ daten$regorderdiff2 <- as.numeric(scale(daten$regorderdiff))
 
 
 
-
 # ---- Size ----
 daten$item_size <- factor(toupper(daten$item_size))
-#table_size <- table(daten$item_size)
 levels(daten$item_size) <- c(levels(daten$item_size), "Other")
-#daten$item_size[is.na(daten$item_size)] <- factor("Other")
-daten$item_size <- ifelse(daten$item_size %in% 
-                            names(prop.table(table(daten$item_size))
-                                  [prop.table(table(daten$item_size))>= 0.01]),
-                          daten$item_size, "Other")
-
+daten$item_size [daten$item_size %in% names(prop.table(table(daten$item_size))
+                         [prop.table(table(daten$item_size))<= 0.01])] <- "Other"
 daten$item_size <- factor(daten$item_size)
-table(daten$item_size)
-prop.table(table(daten$item_size))
 
 
 
 # ---- Color ----
-table_col_clust <- table(daten$item_color)
-levels(daten$item_color) <- c(names(table_col_clust), "Other")
-daten$item_color[is.na(daten$item_color)] <- factor("Other")
-daten[daten$item_color %in% names(table_col_clust)[table_col_clust < 200],"item_color"] <- factor("Other")
-daten$item_color <- factor(daten$item_color)
+levels(daten$item_color) <- c(levels(factor(daten$item_color)), "Other")
+daten$item_color <- agg.col(daten$item_color)
+table(daten$item_color) 
 
-daten$item_colorcat <- as.factor(daten$item_color) # categorisation is very subjective
-daten$item_colorcat[daten$item_colorcat == "?"] <- NA
-daten$item_colorcat <- ifelse(daten$item_colorcat %in% c("?"), NA,
-                              ifelse(daten$item_colorcat %in% c("petrol", "blau", "blue", "azure", "cobalt blue", "dark navy", "darkblue", "turquoise", "silver", "navy", "aqua", "aquamarine", "baltic blue", "darkblue"), "blue",
-                                     ifelse(daten$item_colorcat %in% c("red", "orange", "purple", "currant purple", "pink", "antique pink", "crimson", "bordeaux", "berry", "fuchsia", "perlmutt", "coral", "hibiscus", "magenta", "terracotta", "dark garnet"), "red",
-                                            ifelse(daten$item_colorcat %in% c("green", "olive", "oliv", "dark oliv", "mint", "aubergine", "lemon", "nature", "khaki", "avocado", "jade"), "green",
-                                                   ifelse(daten$item_colorcat %in% c("mocca", "brwon", "brown", "beige", "kanel", "mahagoni", "copper coin", "cognac", "caramel"),"brown",
-                                                          ifelse(daten$item_colorcat %in% c("apricot", "yellow", "vanille", "gold", "ingwer", "white", "mango", "almond", "champagner", "creme", "curry", "ivory", "ocher"), "yellow",
-                                                                 ifelse(daten$item_colorcat %in% c("grey", "black", "dark grey", "graphite", "iron", "habana", "ebony", "amethyst", "basalt", "ash", "ancient", "anthracite", "denim", "dark denim"), "dark",
-                                                                        ifelse(daten$item_colorcat %in% c("striped", "aviator", "pallid", "opal", "stained", "floral", "ecru", "curled"), "others", daten$item_colorcat))))))))
-table(daten$item_colorcat) 
+
+
 # ---- Brand ID ----
 
+
+
 # ---- Price ----
-daten$pricecat <- daten$item_price
-daten$pricecat <- cut(daten$pricecat,c(-1, 0, 20, 50, 100, 200, 400), labels=c(NA, "0-20", "20-50", "50-100", "100-200", "200-400"))
+daten$item_price[daten$item_price <= 0] <- NA
+# daten$pricecat <- daten$item_price
+# daten$pricecat <- cut(daten$pricecat,c(-1, 0, 20, 50, 100, 200, 400), labels=c(NA, "0-20", "20-50", "50-100", "100-200", "200-400"))
+# med_item_price <- median(daten$item_price, na.rm=TRUE)
+# daten$item_price[is.na(daten$item_price)] <- med_item_price
 
-daten$item_price[daten$item_price == 0] <-NA
-med_item_price <- median(daten$item_price, na.rm=TRUE)
-daten$item_price[is.na(daten$item_price)] <- med_item_price
-
+# - Scale Price -
 daten$item_price2 <- as.numeric(scale(daten$item_price))
 #daten$item_price <- Z.outlier(daten$item_price) #Are we sure that 399.95 is outlier?
+
+
+
 # ---- User ID ----
 
-# ---- The number of items user bought within the same day ----
 
+
+# ---- The number of items user bought within the same day ----
 daten <- join(daten, count(daten, c("order_date", "user_id")))
 
+
+
 # ---- Title ----
-daten$user_title[daten$user_title == "not reported"] <- NA # remove not reported titles
-daten$user_title <- as.character(daten$user_title)
-daten$user_title[daten$user_title!='Mrs'] <- 'Other'
-daten$user_title <- as.factor(daten$user_title)
+levels(daten$user_title) <- c(levels(factor(daten$user_title)),"Other")
+daten$user_title[daten$user_title != "Mrs" & daten$user_title != "Mr"] <- factor("Other")
+daten$user_title <- factor(daten$user_title)
 
-table(daten$user_title)
-# ---- Age ----
-daten$age <- age(daten$user_dob,daten$order_date)
-med.age <- round(median(daten$age, na.rm = TRUE))
-daten$age[is.na(daten$age)] <- med.age
-daten$age <- Z.outlier(daten$age)
 
-daten$agecat <- recode(daten$age, "0:27='18-27';28:37='28-37'; 38:47='38-47'; 48:57='48-57'; 58:67='58-67'; 68:77='68-77'; 78:87='78-87'; 88:100='88-100'")
-daten$age2 <- as.numeric(scale(daten$age))
 
-table(daten$agecat)
-summary(daten$age)
-hist(daten$age)
-boxplot(daten$age)
 # ---- User State ----
+
+# ----------------------- End existing variable prep
+
+
+
+
+# ----------------------- New Variables
+
+# ---- 1-3: Date variables ----
+daten$order_month <- as.factor(months(daten$order_date)) # new column with month of delivery
+daten$delivery_time <- Z.outlier(as.numeric(daten$delivery_date - daten$order_date))
+daten$regorderdiff <- as.numeric(daten$order_date - daten$user_reg_date) ## makes sense?
+# If necessary replace NAs by median
+#MED_DEL <- round( median (daten$delivery_time, na.rm =TRUE)) # round( mean (daten$delivery_time, na.rm =TRUE)) gives median of 4 and mean of 11 for delivery time
+#daten$delivery_date[is.na(daten$delivery_date)]<- daten$order_date[is.na(daten$delivery_date)] + MED_DEL
+#daten$delivery_time[is.na(daten$delivery_time)] <- MED_DEL
+
+# Scaled date variables
+daten$delivery_time2 <- as.numeric(scale(daten$delivery_time))
+daten$regorderdiff2 <- as.numeric(scale(daten$regorderdiff))
+
+
+
+# ---- 4: Customer age ----
+daten$age <- age(daten$user_dob,daten$order_date)
+daten$age <- Z.outlier(daten$age)
+#med.age <- round(median(daten$age, na.rm = TRUE))
+#daten$age[is.na(daten$age)] <- med.age
+#daten$agecat <- recode(daten$age, "0:27='18-27';28:37='28-37'; 38:47='38-47'; 48:57='48-57'; 58:67='58-67'; 68:77='68-77'; 78:87='78-87'; 88:100='88-100'")
+#daten$age2 <- as.numeric(scale(daten$age))
+
+
+
+
+
+
+
+
 
 
 missmap(daten, main = "Missing values vs observed") # to give a plot of the missing values per variable
