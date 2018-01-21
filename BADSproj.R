ae7c15356a6bc2574ec36a0a8fdd9afa91d7042e
aydinsena <senaaydin484@gmail.com>
2018-01-21 12:21:18 +0100

    new variables

--- BADSproj.R
+++ BADSproj.R
@@ -84,6 +84,14 @@ Z.outlier <- function(x){
   x[Zscore <(-3)] <- NA
   return(x)}
 
+#-Duplicated rows
+count.duplicates <- function(DF){
+  x <- do.call('paste', c(DF, sep = '\r'))
+  ox <- order(x)
+  rl <- rle(x[ox])
+  cbind(DF[ox[cumsum(rl$lengths)],,drop=FALSE],count = rl$lengths)
+  
+}
 
 # - Aggregate color levels -
 agg.col <- function (df.col) {
@@ -148,10 +156,6 @@ daten$item_price2 <- as.numeric(scale(daten$item_price))
 
 
 
-# ---- The number of items user bought within the same day ----
-daten <- join(daten, count(daten, order_date, user_id),by = c("order_date", "user_id"))
-
-
 
 # ---- Title ----
 levels(daten$user_title) <- c(levels(factor(daten$user_title)),"Other")
@@ -194,17 +198,21 @@ daten$age <- Z.outlier(daten$age)
 
 
 # ---- Number of items in a basket ----
-
+daten <- join(daten, dplyr::count(daten, order_date, user_id),by = c("order_date", "user_id"))
 
 
 # ---- Count same item (with different size/color) in same basket ----
 
 
-
 # ---- Customers past return rates ----
 
-# ----------------------- End new variables
+rate<-data.frame(daten$user_id)
+sena<-count.duplicates(rate)
 
+
+
+
+# ----------------------- End new variables
 
 
 
