4c34d6157fc1cc63d36c030ce8036f412bd6bdf2
aydinsena <senaaydin484@gmail.com>
2018-01-28 14:12:25 +0100

    I am not done yet

--- AdapToCSL.R
+++ AdapToCSL.R
@@ -1,1 +1,13 @@
 # file for Adaptation to Cost Sensitive Learning
+meanItemPrice<-mean(daten$item_price)
+a<-0
+b<-0.5*-meanItemPrice
+c<-2.5*-(3+0.1*meanItemPrice)
+d<-0
+
+costs <- matrix(c(a,b,c,d), 2)
+colnames(costs) <- rownames(costs) <-levels(daten$return)
+
+#theoretical threshold
+th <- costs[2,1]/(costs[2,1] + costs[1,2])
+
