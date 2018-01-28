beeb8f43d5095bf583e1d73d35def23e2357b8bf
BanjoBasti <s.guetgemann@gmx.net>
2018-01-28 13:42:04 +0100

    Mal was neues

--- BADSproj.R
+++ BADSproj.R
@@ -101,12 +101,12 @@ Z.outlier <- function(x){
 agg.col <- function (df.col) {
   df.col <- factor(df.col)
   df.col <- ifelse(df.col %in% c("?"), "Other",
-                             ifelse(df.col %in% c("petrol", "blau", "blue", "azure", "cobalt blue", "dark navy", "darkblue", "turquoise", "silver", "navy", "aqua", "aquamarine", "baltic blue", "darkblue"), "blue",
-                                    ifelse(df.col %in% c("red", "orange", "purple", "currant purple", "pink", "antique pink", "crimson", "bordeaux", "berry", "fuchsia", "perlmutt", "coral", "hibiscus", "magenta", "terracotta", "dark garnet"), "red",
-                                           ifelse(df.col %in% c("green", "olive", "oliv", "dark oliv", "mint", "aubergine", "lemon", "nature", "khaki", "avocado", "jade"), "green",
-                                                  ifelse(df.col %in% c("mocca", "brwon", "brown", "beige", "kanel", "mahagoni", "copper coin", "cognac", "caramel"),"brown",
-                                                         ifelse(df.col %in% c("apricot", "yellow", "vanille", "gold", "ingwer", "white", "mango", "almond", "champagner", "creme", "curry", "ivory", "ocher"), "yellow",
-                                                                ifelse(df.col %in% c("grey", "black", "dark grey", "graphite", "iron", "habana", "ebony", "amethyst", "basalt", "ash", "ancient", "anthracite", "denim", "dark denim"), "dark", "Other")))))))
+                   ifelse(df.col %in% c("petrol", "blau", "blue", "azure", "cobalt blue", "dark navy", "darkblue", "turquoise", "silver", "navy", "aqua", "aquamarine", "baltic blue", "darkblue"), "blue",
+                          ifelse(df.col %in% c("red", "orange", "purple", "currant purple", "pink", "antique pink", "crimson", "bordeaux", "berry", "fuchsia", "perlmutt", "coral", "hibiscus", "magenta", "terracotta", "dark garnet"), "red",
+                                 ifelse(df.col %in% c("green", "olive", "oliv", "dark oliv", "mint", "aubergine", "lemon", "nature", "khaki", "avocado", "jade"), "green",
+                                        ifelse(df.col %in% c("mocca", "brwon", "brown", "beige", "kanel", "mahagoni", "copper coin", "cognac", "caramel"),"brown",
+                                               ifelse(df.col %in% c("apricot", "yellow", "vanille", "gold", "ingwer", "white", "mango", "almond", "champagner", "creme", "curry", "ivory", "ocher"), "yellow",
+                                                      ifelse(df.col %in% c("grey", "black", "dark grey", "graphite", "iron", "habana", "ebony", "amethyst", "basalt", "ash", "ancient", "anthracite", "denim", "dark denim"), "dark", "Other")))))))
   df.col <- factor(df.col)}
 
 
@@ -127,7 +127,7 @@ daten$delivery_date[daten$order_date>daten$delivery_date] <-NA
 daten$item_size <- factor(toupper(daten$item_size))
 levels(daten$item_size) <- c(levels(daten$item_size), "Other")
 daten$item_size [daten$item_size %in% names(prop.table(table(daten$item_size))
-                         [prop.table(table(daten$item_size))<= 0.01])] <- "Other"
+                                            [prop.table(table(daten$item_size))<= 0.01])] <- "Other"
 daten$item_size <- factor(daten$item_size)
 
 
@@ -177,14 +177,10 @@ daten$user_title <- factor(daten$user_title)
 # ---- Date dependent variables ----
 daten$order_month <- as.factor(months(daten$order_date)) # new column with month of delivery
 daten$delivery_time <- Z.outlier(as.numeric(daten$delivery_date - daten$order_date))
-daten$regorderdiff <- as.numeric(daten$order_date - daten$user_reg_date) ## makes sense?
+daten$regorderdiff <- as.numeric(daten$order_date - daten$user_reg_date)
 # If necessary replace NAs by median
-#MED_DEL <- round( median (daten$delivery_time, na.rm =TRUE)) # round( mean (daten$delivery_time, na.rm =TRUE)) gives median of 4 and mean of 11 for delivery time
+#MED_DEL <- round( median (daten$delivery_time, na.rm =TRUE)) gives median of 4 and mean of 11 for delivery time
 #daten$delivery_time[is.na(daten$delivery_time)] <- MED_DEL
-
-# Scaled date variables
-#daten$delivery_time2 <- as.numeric(scale(daten$delivery_time))
-#daten$regorderdiff2 <- as.numeric(scale(daten$regorderdiff))
 
 
 
@@ -193,17 +189,18 @@ daten$age <- age(daten$user_dob,daten$order_date)
 daten$age <- Z.outlier(daten$age)
 med.age <- median(daten$age, na.rm = TRUE)
 daten$age[is.na(daten$age)] <- med.age
-#daten$age2 <- as.numeric(scale(daten$age))
 
 
 
 # ---- Number of items in a basket ----
-daten <- join(daten, dplyr::count(daten, order_date, user_id),by = c("order_date", "user_id"))
+daten <- as.numeric(join(daten, dplyr::count(daten, order_date, user_id),
+                         by = c("order_date", "user_id")))
 
 
 
 # ---- The number of same item user bought within the same basket ----
-daten <- join(daten, count(daten, order_date, user_id, item_id),by = c("order_date", "user_id","item_id"))
+daten <- as.numeric(join(daten, count(daten, order_date, user_id, item_id),
+                         by = c("order_date", "user_id","item_id")))
 names(daten)[names(daten) == "n"] <- "ct_basket_size"
 names(daten)[names(daten) == "nn"] <- "ct_same_items"
 
@@ -218,7 +215,7 @@ returnsAndOrders <- join(numberOfOrdersPerUser, numberOfReturnsPerUser, by = "us
 returnsAndOrders$TotalReturn[is.na(returnsAndOrders$TotalReturn)]<-0 #replace NA by 0
 returnsAndOrders$customersReturnRate<-returnsAndOrders[,3]/returnsAndOrders[,2]
 daten<-merge(daten, returnsAndOrders[, c("user_id", "customersReturnRate")], by="user_id")
-  
+
 
 
 
@@ -258,21 +255,31 @@ set.seed(123)
 idx.train <- createDataPartition(y = daten$return, p = 0.75, list = FALSE) # Draw a random, stratified sample including p percent of the data
 test <-  daten[-idx.train, ] # test set
 train <- daten[idx.train, ] # training set
+
+# - Extra Split for WoE -
+woe.idx.train <- createDataPartition(y=train$return, p = 0.7, list = FALSE) # 0.7 just choosen randomly at the moment
+train.split <- train[woe.idx.train,] # Set for WoE calculation
+
 # ----------------------- End: Split Data
+
+
+
+
 # ----------> should the data be splitted before? (see row 257)
 
 
 
 
 # ----------------------- Start: WoE
+# Probably it makes sense to apply WoE only to variables with many factors
 #tapply(train$item_id, train$return, summary)
-#tapply(train$item_size, train$return, summary)
-#tapply(train$item_color, train$return, summary)
+tapply(train$item_size, train$return, summary)
+tapply(train$item_color, train$return, summary)
 #tapply(train$brand_id, train$return, summary)
 #tapply(train$item_price, train$return, summary)
 #tapply(train$user_id, train$return, summary)
-#tapply(train$user_title, train$return, summary)
-#tapply(train$user_state, train$return, summary)
+tapply(train$user_title, train$return, summary)
+tapply(train$user_state, train$return, summary)
 #tapply(train$order_month, train$return, summary)
 #tapply(train$delivery_time, train$return, summary)
 #tapply(train$regorderdiff, train$return, summary)
@@ -280,7 +287,7 @@ train <- daten[idx.train, ] # training set
 #tapply(train$user_title, train$return, summary)
 #tapply(train$ct_basket_size, train$return, summary)
 #tapply(train$ct_same_items, train$return, summary)
-woe.values <- woe(return ~ ., data=train, zeroadj=0.1)
+woe.values <- woe(return ~ ., data=train.split, zeroadj=0.1)
 # - note: klaR assumes the first level of target to be the target level (WoE refer to no returns)
 
 ## - check for plausibility by plotting weights against their levels
@@ -295,6 +302,25 @@ train.woe <- predict(woe.values, newdata=train, replace=TRUE)
 
 
 
+
+# ----------------------- Start: Prep Input for NN
+
+# normalized nn are better
+# Be careful to use the training mean/sd for normalization
+normalizer <- caret::preProcess(train.woe[,names(train.woe) %in% c("item_price","regorderdiff","age","ct_basket_size","ct_same_items")],
+                                method = c("center", "scale"))
+nn.train.woe <- predict(normalizer, newdata = train.woe)
+nn.test.woe <- predict(normalizer, newdata = test.woe)
+
+# - Adjust return values for Neural Network
+nn.train.woe$return <- as.factor(ifelse(nn.train.woe$return == 1, 1, -1))
+nn.test.woe$return <- as.factor(ifelse(nn.test.woe$return == 1, 1, -1))
+
+# ----------------------- End: Prep Input for NN
+
+
+
+
 #missmap(daten, main = "Missing values vs observed") # to give a plot of the missing values per variable
 
 # ---- IDEAS & OPEN QUESTIONS ----
@@ -302,8 +328,7 @@ train.woe <- predict(woe.values, newdata=train, replace=TRUE)
 #    I If a nominal or ordinal column in the test data set contains more levels than the corresponding column in the training data set, you can add levels to the column in the training data set manually using the following command:
 #     training_data$salutation = factor(training_data$salutation, levels=c(levels(training_data$salutation), "Family"))
 #   II Weekday?
-#  III Clever to bin delivery time? More than a week will appear huge to the customer, no matter how long exact
-#   IV Order nominal factors to receive ordinal?
+#  IV Order nominal factors to receive ordinal?
 
 # 1)  In case someone buys more stuff at once, the whole thing should obviously not be 
 #     discouraged because one or two items are likely to be returned
@@ -312,15 +337,6 @@ train.woe <- predict(woe.values, newdata=train, replace=TRUE)
 #     would be lost if discouraged and wouldnt have been returned. It is worse if cheap items
 #     are returned than expensive ones, because percentage wise the return is more expensive
 #     as well as its 3€ plus 10 % ov value.
-# 3)  Maybe other categories as alternative to binning? a) dark-light contrasting
-#     b) warm-cold contrast c) black-white contrast ... are dark colors returned more in summertime?
-#     combining fasion colors with months and returns? colors  can look differently online.
 # 4)  likelihood ratio test; AIC; step function (direction: both)
-# 5)  Without knowing the item, statements about price or size can hardly be made
-# 6)  daten <- daten(na.strings = c("?","not reported") --> question marks and not reported entries havent been replaced with NAs yet
 # 7)  review at the end which packages we really need
-# 8)  was is on purpose that user title and user state have not been class-transformed?
-# 9) do we really want to put all titles other than MRS as "other" ?
-# 10) division into rarely returned/never returned should rather not be included
-# 11) items with price zero should not be available to be returned as it would costwise not make sense. they should not simply be put as NAs
 # 12) text should include that when you are the shopowner and you set up this analysis, some open questions could be answered with more background knowlegde
