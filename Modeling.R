850630f46d2394f50cdc2b5947ec19c68847b453
BanjoBasti <s.guetgemann@gmx.net>
2018-01-29 09:55:28 +0100

    Finito 1.0

--- Modeling.R
+++ Modeling.R
@@ -27,68 +27,116 @@ yhat <- list()
 auc <- list()
 
 tuneControl <- makeTuneControlGrid(resolution = 3, tune.threshold = FALSE)
-
-# ----------------------- 
-featureSearchCtrl <- makeFeatSelControlSequential(method="sfs", alpha = 0.01) 
-
 # - Define resampling procedure (here: 5-fold cross validation) -
 rdesc <- makeResampleDesc(method="CV", iters=5, stratify=TRUE)
 task <- makeClassifTask(data=train.woe, target="return", positive="1")
+
+# ----------------------- 
+
+
+
 
 
 # ----------------------- Start Tuning
 # ----------------------- # ----------------------- # ----------------------- # ----------------------- 
 
-# ----------------------- Random Forest
+# ----------------------- Start: Random Forest with wrapper
 
+# - Set parameter, task, tune and update learner -
 set.seed(123)
-
 rf.task <- makeClassifTask(data=rf.train.woe, target="return", positive="1")
-
-rf.parms <- makeParamSet(
+rfw.parms <- makeParamSet(
   # The recommendation for mtry by Breiman is squareroot number of columns
   makeIntegerParam("mtry", lower = round(sqrt(ncol(rf.train.woe))/2),
                    upper = round(sqrt(ncol(rf.train.woe))*2)), # Number of features selected at each node, smaller -> faster
   makeIntegerParam("ntree", lower = 100, upper = 1000) # Number of tree, smaller -> faster
 ) 
 
-# mtry from half of squaretoot to 4 times squareroot
-#the space of number of trees in the forest to 100, 250, 500, 750, 1000 
+# mtry from half of squaretoot to 2 times squareroot
 # number of bagging iterations 5, 10, 15, 20, 25
-
 parallelStartSocket(3, level = "mlr.tuneParams")
-rf.tuning <- tuneParams(rf, task = rf.task, resampling = rdesc,
+rfw.tuning <- tuneParams(rf, task = rf.task, resampling = rdesc,
+                        par.set = rfw.parms, control = tuneControl, measures = mlr::auc)
+parallelStop()
+rfw.tuning$x
+rfw.tuning <- setHyperPars(rf, par.vals = rfw.tuning$x) 
+
+# - Train, predict, AUC -
+modelLib[["rf_wrap"]] <- mlr::train(rfw.tuning, task = rf.task)
+yhat[["rf_wrap"]] <- predict(modelLib[["rf_wrap"]], newdata = rf.test.woe)
+auc[["rf_wrap"]] <- mlr::performance(yhat[["rf_wrap"]], measures = mlr::auc)
+auc
+
+# ----------------------- End: Random Forest with wrapper
+
+
+
+
+# ----------------------- Start: Random Forest without wrapper
+
+# - Set parameter, task, tune and update learner -
+set.seed(123)
+rf.parms <- makeParamSet(
+  makeIntegerParam("mtry", lower = round(sqrt(ncol(train.woe))/2),
+                   upper = round(sqrt(ncol(train.woe))*2)), 
+  makeIntegerParam("ntree", lower = 100, upper = 1000)
+) 
+parallelStartSocket(3, level = "mlr.tuneParams")
+rf.tuning <- tuneParams(rf, task = task, resampling = rdesc,
                         par.set = rf.parms, control = tuneControl, measures = mlr::auc)
 parallelStop()
-# Start resampling
 rf.tuning$x
-rf.tuning <- setHyperPars(rf, par.vals = rf.tuning$x) # necessary or how is tuned data extracted?
+rf.tuning <- setHyperPars(rf, par.vals = rf.tuning$x)
 
-# Train the model on the full training data (not only a CV-fold)
-modelLib[["rf"]] <- mlr::train(rf.tuning, task = rf.task)
-
-# Make prediction on test data
-yhat[["rf"]] <- predict(modelLib[["rf"]], newdata = rf.test.woe)
-
-
-# Calculate AUC performance on test set 
+# - Train, predict, AUC -
+modelLib[["rf"]] <- mlr::train(rf.tuning, task = task)
+yhat[["rf"]] <- predict(modelLib[["rf"]], newdata = test.woe)
 auc[["rf"]] <- mlr::performance(yhat[["rf"]], measures = mlr::auc)
 auc
-# ----------------------- End: Random Forest
+
+# ----------------------- End: Random Forest without wrapper
 
 
 
 
-# ----------------------- Start: Logistic Regression
 
+
+
+# ----------------------- Start: Logistic Regression wrapped
+
+# - Set parameter, task, tune and update learner -
 set.seed(123)
-
+lr.train.woe <- train.woe[,names(train.woe) %in% c("return","woe.user_id", "woe.item_id")]
 lr.task <- makeClassifTask(data=lr.train.woe, target="return", positive="1")
 
 lr.parms <- makeParamSet(
-  makeIntegerParam("lambda1", lower = 0, upper =200) 
+  makeNumericParam("lambda1", lower = 0, upper =200) 
 ) 
+parallelStartSocket(3, level = "mlr.tuneParams")
+lrw.tuning <- tuneParams(lr, task = lr.task, resampling = rdesc,
+                        par.set = lr.parms, control = tuneControl, measures = mlr::auc)
+parallelStop()
+lrw.tuning$x
+lrw.tuning <- setHyperPars(lr, par.vals = lrw.tuning$x) # necessary or how is tuned data extracted?
 
+# - Train, predict, AUC 
+modelLib[["lr_wrap"]] <- mlr::train(lrw.tuning, task = lr.task)
+yhat[["lr_wrap"]] <- predict(modelLib[["lr_wrap"]], newdata = lr.test.woe)
+auc[["lr_wrap"]] <- mlr::performance(yhat[["lr_wrap"]], measures = mlr::auc)
+auc
+
+# ----------------------- End: Logistic Regression
+
+
+
+
+# ----------------------- Start: Logistic Regression no wrap
+
+# - Set parameter, task, tune and update learner -
+set.seed(123)
+lr.parms <- makeParamSet(
+  makeNumericParam("lambda1", lower = 0, upper =200) 
+) 
 parallelStartSocket(3, level = "mlr.tuneParams")
 lr.tuning <- tuneParams(lr, task = task, resampling = rdesc,
                         par.set = lr.parms, control = tuneControl, measures = mlr::auc)
@@ -96,14 +144,9 @@ parallelStop()
 lr.tuning$x
 lr.tuning <- setHyperPars(lr, par.vals = lr.tuning$x) # necessary or how is tuned data extracted?
 
-# Train the model on the full training data (not only a CV-fold)
-modelLib[["lr"]] <- mlr::train(rf.tuning, task = task)
-
-# Make prediction on test data
+# - Train, predict, AUC -
+modelLib[["lr"]] <- mlr::train(lr.tuning, task = task)
 yhat[["lr"]] <- predict(modelLib[["lr"]], newdata = test.woe)
-
-
-# Calculate AUC performance on test set 
 auc[["lr"]] <- mlr::performance(yhat[["lr"]], measures = mlr::auc)
 auc
 
@@ -112,80 +155,68 @@ auc
 
 
 
+
+# ----------------------- Start: Neural Networks
+
+# - Set parameter, task, tune and update learner -
+set.seed(123)
+nn.task <- makeClassifTask(data=nn.train.woe, target="return", positive="1")
+nn.parms <- makeParamSet(
+  makeNumericParam("decay", lower = -4, upper = 0, trafo = function(x) 10^x), 
+  makeDiscreteParam("size", values = c(1,2,4,8,16)),
+  makeIntegerParam("maxit", lower= 200, upper=800)) 
+# number of neurons in the hidden layer  
+# decay from 0.0001 to 1
+parallelStartSocket(3)
+nn.tuning <- tuneParams(nn, task = nn.task, resampling = rdesc,
+                        par.set = nn.parms, control = tuneControl, measures = mlr::auc)
+parallelStop()
+nn <- setHyperPars(nn, par.vals = nn.tuning$x)
+
+# - Train, predict, AUC -
+modelLib[["nn"]] <- mlr::train(nn, task = nn.task)
+yhat[["nn"]] <- predict(modelLib[["nn"]], newdata = nn.test.woe)
+auc[["nn"]] <- mlr::performance(yhat[["nn"]], measure = mlr::auc)
+auc
+
+# ----------------------- End: Neural Networks
+
+
+
+
 # ----------------------- Start: Extreme Gradient Boosting
 
+# - Set parameter, task, tune and update learner -
 set.seed(123)
-
 xgb.parms <- makeParamSet(
   makeIntegerParam("nrounds", lower= 100,upper = 200), 
   makeIntegerParam("max_depth", lower= 3, upper= 10), 
   makeNumericParam("eta", lower = 0.01, upper = 0.5), 
   makeNumericParam("gamma", lower =0, upper= 0.3),
-  makeDiscreteParam("colsample_bytree", values = c(0.6, 0.7, 0.8)),
+  makeNumericParam("colsample_bytree", lower= 0.6, upper= 0.8),
   makeNumericParam("min_child_weight",lower=0.8,upper=2),
   makeNumericParam("subsample", lower= 0.8, upper=1)
-  
 )
 # choose lambda and alpha randomly - how?
 # currently default: lambda = 1, alpha= 0
 # eta went until 0.15 in excercise
 # nrounds = number of iterations/trees?
-
-
-
 parallelStartSocket(3, level = "mlr.tuneParams")
-
 xgb.tuning <- tuneParams(xgb, task = task, resampling = rdesc,
                          par.set = xgb.parms, control = tuneControl, measures = mlr::auc)
-
 parallelStop()
-# Extract optimal parameter values after tuning 
 xgb.tuning$x
-
-# Update the learner to the optimal hyperparameters
 xgb <- setHyperPars(xgb, par.vals = c(xgb.tuning$x, "verbose" = 0)) # What is verbose and do we need to do that shit?
-xgb
 
-# Train the model on the full training data (not only a CV-fold)
+# - Train, predict, AUC -
 modelLib[["xgb"]] <- mlr::train(xgb, task = task)
-
-# Make prediction on test data
 yhat[["xgb"]] <- predict(modelLib[["xgb"]], newdata = test.woe)
-
-# Calculate AUC performance on test set 
 auc[["xgb"]] <- mlr::performance(yhat[["xgb"]], measures = mlr::auc)
 auc
+
 # ----------------------- End: Extreme Gradient Boosting
 
 
-
-
-# ----------------------- Start: Neural Networks
-
-set.seed(123)
-
-nn.task <- makeClassifTask(data=nn.train.woe, target="return", positive="1")
-
-# Neural networks work better when the data inputs are on the same scale, e.g. standardized
-nn.parms <- makeParamSet(
-  makeNumericParam("decay", lower = -4, upper = 0, trafo = function(x) 10^x), 
-  makeDiscreteParam("size", values = c(1,2,4,8,16))) 
-# number of neurons in the hidden layer  
-#decay from 0.0001 to 1
-
-parallelStartSocket(3)
-nn.tuning <- tuneParams(nn, task = nn.task, resampling = rdesc,
-                        par.set = nn.parms, control = tuneControl, measures = mlr::auc)
-parallelStop()
-
-nn <- setHyperPars(nn, par.vals = nn.tuning$x)
-
-modelLib[["nn"]] <- mlr::train(nn, task = nn.task)
-
-yhat[["nn"]] <- predict(modelLib[["nn"]], newdata = nn.test.woe)
-
+# ... Stuff ...
 # to use different kinds of performances:
 #mlr::performance(pred_raw, measures = list(mlr::auc, mlr::brier, mlr::acc))
-auc[["nn"]] <- mlr::performance(yhat[["nn"]], measure = mlr::auc)
-auc
-# ----------------------- End: Neural Networks
