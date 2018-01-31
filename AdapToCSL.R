e3ec8f9dcda480e941b3c4f61009dcd94da48230
KaiRstudio <playdesseb.de>
2018-01-31 09:10:57 +0100

    X

--- AdapToCSL.R
+++ AdapToCSL.R
@@ -11,3 +11,11 @@ colnames(costs) <- rownames(costs) <-levels(daten$return)
 #theoretical threshold
 th <- costs[2,1]/(costs[2,1] + costs[1,2])
 
+# needs to be done for each model and for each threshold --> minimised costs wanted
+falsely.not.warned <- sum(3+0.1(daten$item_price[no warning, returned]))
+falsely.not.warned <- sum(3+0.1(daten$item_price[daten$ProbabilityOfReturn<ThresholdForReturns & daten$return==1]))
+
+falsely.warned <- sum(daten$item_price[warning, kept])
+falsely.warned <- sum(daten$item_price[daten$ProbabilityOfReturn>ThresholdForReturn &  daten$return==0])
+
+exp.costs <- 2.5*falsely.not.warned + 0.5*falsely.warned
