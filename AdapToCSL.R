f8992c2959b726aad883b34b59bb663a03b4d9a0
KaiRstudio <playdesseb.de>
2018-02-03 13:06:41 +0100

    cv sets replaced

--- AdapToCSL.R
+++ AdapToCSL.R
@@ -44,29 +44,31 @@ caret::confusionMatrix(xgb.pred.class, test.woe$return)
 
 # ----------------------- start cost calculations
 
+testset.1 <- test.2
+testset.1$pred <- rf.cat.pred$data$prob.1
+
 # calculate empirical cost-dependent threshold
-abcdefg <- function(rf, test, item_price, x){
+abcdefg <- function(rf, cv_train, item_price, x){
   all_cost <- numeric()
   for (i in seq_along(x)){
-    all_cost[i] <- (2.5*sum(3+0.1*(test.2$item_price[rf.cat.pred$data[,3]<x[i] & test.2$return==1]))+0.5*sum(test.2$item_price[rf.cat.pred$data[,3]>x[i] & test.2$return==0]));
+    all_cost[i] <- (2.5*sum(3+0.1*(cv_train$item_price[cv_train$pred<x[i] & cv_train$return==1]))+0.5*sum(cv_train$item_price[cv_train$pred>x[i] & cv_train$return==0]));
   }
   opt_cutoff <- x[which.min(all_cost)]
   return (opt_cutoff)
     }
 
 # Cross validated calculation of optimal, empirical threshold
-xxx <- seq(from =0.1,to= 0.9, by = 0.001)
+xxx <- seq(from =0.1,to= 0.9, by = 0.01)
 
 k <- 10
-folds <- cut(1:nrow(test.2), breaks = k, labels = FALSE)
+folds <- cut(1:nrow(testset.1), breaks = k, labels = FALSE)
 set.seed(123)
 folds <- sample(folds)
 cv_results <- matrix(nrow = 1, ncol = k)
 for (j in 1:k) {
   idx_val <- which(folds == j, arr.ind = TRUE)
-  cv_train <- test.2[-idx_val,]
-  cv_val <- test.2[idx_val,]
-  cv_results[1,j] <- abcdefg(rf, test, item_price, xxx)
+  cv_train <- testset.1[-idx_val,]
+  cv_results[1,j] <- abcdefg(rf, cv_train, item_price, xxx)
 }
 opt.cross.val.th1 <- mean(cv_results)
 
@@ -86,11 +88,14 @@ exp.costs.opt1 <- (2.5*falsely.not.warned.opt1 + 0.5*falsely.warned.opt2)
 
 # ---------------------------------------------------------------------
 
+testset.2 <- test.woe
+testset.2$pred <- rf.pred$data$prob.1
+
 # calculate empirical cost-dependent threshold
-abcdefg2 <- function(rf, test, item_price, x){
+abcdefg2 <- function(rf, cv_train, item_price, x){
   all_cost <- numeric()
   for (i in seq_along(x)){
-    all_cost[i] <- (2.5*sum(3+0.1*(test.woe$item_price[rf.pred$data[,3]<x[i] & test.woe$return==1]))+0.5*sum(test.woe$item_price[rf.pred$data[,3]>x[i] & test.woe$return==0]));
+    all_cost[i] <- (2.5*sum(3+0.1*(cv_train$item_price[cv_train$pred<x[i] & cv_train$return==1]))+0.5*sum(cv_train$item_price[cv_train$pred>x[i] & cv_train$return==0]));
   }
   opt_cutoff <- x[which.min(all_cost)]
   return (opt_cutoff)
@@ -100,15 +105,14 @@ abcdefg2 <- function(rf, test, item_price, x){
 xxx <- seq(from =0.1,to= 0.9, by = 0.001)
 
 k <- 10
-folds <- cut(1:nrow(test.woe), breaks = k, labels = FALSE)
+folds <- cut(1:nrow(testset.2), breaks = k, labels = FALSE)
 set.seed(123)
 folds <- sample(folds)
 cv_results2 <- matrix(nrow = 1, ncol = k)
 for (j in 1:k) {
   idx_val <- which(folds == j, arr.ind = TRUE)
-  cv_train <- test.woe[-idx_val,]
-  cv_val <- test.woe[idx_val,]
-  cv_results2[1,j] <- abcdefg2(rf, test, item_price, xxx)
+  cv_train <- testset.2[-idx_val,]
+  cv_results2[1,j] <- abcdefg2(rf, cv_train, item_price, xxx)
 }
 opt.cross.val.th2 <- mean(cv_results2)
 
@@ -128,11 +132,14 @@ exp.costs.opt2 <- (2.5*falsely.not.warned.opt3 + 0.5*falsely.warned.opt3)
 
 # ---------------------------------------------------------------------
 
+testset.3 <- test.woe
+testset.3$pred <- lr.pred$data$prob.1
+
 # calculate empirical cost-dependent threshold
-abcdefg3 <- function(lr, test, item_price, x){
+abcdefg3 <- function(lr, cv_train, item_price, x){
   all_cost <- numeric()
   for (i in seq_along(x)){
-    all_cost[i] <- (2.5*sum(3+0.1*(test.woe$item_price[lr.pred$data[,3]<x[i] & test.woe$return==1]))+0.5*sum(test.woe$item_price[lr.pred$data[,3]>x[i] & test.woe$return==0]));
+    all_cost[i] <- (2.5*sum(3+0.1*(cv_train$item_price[cv_train$pred<x[i] & cv_train$return==1]))+0.5*sum(cv_train$item_price[cv_train$pred>x[i] & cv_train$return==0]));
   }
   opt_cutoff <- x[which.min(all_cost)]
   return (opt_cutoff)
@@ -142,15 +149,14 @@ abcdefg3 <- function(lr, test, item_price, x){
 xxx <- seq(from =0.1,to= 0.9, by = 0.001)
 
 k <- 10
-folds <- cut(1:nrow(test.woe), breaks = k, labels = FALSE)
+folds <- cut(1:nrow(testset.3), breaks = k, labels = FALSE)
 set.seed(123)
 folds <- sample(folds)
 cv_results3 <- matrix(nrow = 1, ncol = k)
 for (j in 1:k) {
   idx_val <- which(folds == j, arr.ind = TRUE)
-  cv_train <- test.woe[-idx_val,]
-  cv_val <- test.woe[idx_val,]
-  cv_results3[1,j] <- abcdefg3(lr, test, item_price, xxx)
+  cv_train <- testset.3[-idx_val,]
+  cv_results3[1,j] <- abcdefg3(lr, cv_train, item_price, xxx)
 }
 opt.cross.val.th3 <- mean(cv_results3)
 
@@ -170,11 +176,14 @@ exp.costs.opt3 <- (2.5*falsely.not.warned.opt5 + 0.5*falsely.warned.opt5)
 
 # ---------------------------------------------------------------------
 
+testset.4 <- test.2
+testset.4$pred <- lr.cat.pred$data$prob.1
+
 # calculate empirical cost-dependent threshold
-abcdefg4 <- function(lr, test, item_price, x){
+abcdefg4 <- function(lr, cv_train, item_price, x){
   all_cost <- numeric()
   for (i in seq_along(x)){
-    all_cost[i] <- (2.5*sum(3+0.1*(test.2$item_price[lr.cat.pred$data[,3]<x[i] & test.2$return==1]))+0.5*sum(test.2$item_price[lr.cat.pred$data[,3]>x[i] & test.2$return==0]));
+    all_cost[i] <- (2.5*sum(3+0.1*(cv_train$item_price[cv_train$pred<x[i] & cv_train$return==1]))+0.5*sum(cv_train$item_price[cv_train$pred>x[i] & cv_train$return==0]));
   }
   opt_cutoff <- x[which.min(all_cost)]
   return (opt_cutoff)
@@ -184,15 +193,14 @@ abcdefg4 <- function(lr, test, item_price, x){
 xxx <- seq(from =0.1,to= 0.9, by = 0.001)
 
 k <- 10
-folds <- cut(1:nrow(test.2), breaks = k, labels = FALSE)
+folds <- cut(1:nrow(testset.4), breaks = k, labels = FALSE)
 set.seed(123)
 folds <- sample(folds)
 cv_results4 <- matrix(nrow = 1, ncol = k)
 for (j in 1:k) {
   idx_val <- which(folds == j, arr.ind = TRUE)
-  cv_train <- test.2[-idx_val,]
-  cv_val <- test.2[idx_val,]
-  cv_results4[1,j] <- abcdefg4(lr, test, item_price, xxx)
+  cv_train <- testset.4[-idx_val,]
+  cv_results4[1,j] <- abcdefg4(lr, cv_train, item_price, xxx)
 }
 opt.cross.val.th4 <- mean(cv_results4)
 
@@ -212,13 +220,15 @@ exp.costs.opt4 <- (2.5*falsely.not.warned.opt7 + 0.5*falsely.warned.opt7)
 
 # ---------------------------------------------------------------------
 
-nn.test.woe2$price <- test.woe$item_price
+testset.4 <- nn.test.woe
+testset.4$pred <- nn.pred$data$prob.1
+testset.4$price <- test.woe$item_price
 
 # calculate empirical cost-dependent threshold
-abcdefg5 <- function(nn, test, item_price, x){
+abcdefg5 <- function(nn, cv_train, item_price, x){
   all_cost <- numeric()
   for (i in seq_along(x)){
-    all_cost[i] <- (2.5*sum(3+0.1*(nn.test.woe2$price[nn.pred$data[,3]<x[i] & nn.test.woe2$return==1]))+0.5*sum(nn.test.woe2$price[nn.pred$data[,3]>x[i] & nn.test.woe2$return==0]));
+    all_cost[i] <- (2.5*sum(3+0.1*(cv_train$price[cv_train$pred<x[i] & cv_train$return==1]))+0.5*sum(cv_train$price[cv_train$pred>x[i] & cv_train$return==0]));
   }
   opt_cutoff <- x[which.min(all_cost)]
   return (opt_cutoff)
@@ -228,15 +238,14 @@ abcdefg5 <- function(nn, test, item_price, x){
 xxx <- seq(from =0.1,to= 0.9, by = 0.001)
 
 k <- 10
-folds <- cut(1:nrow(nn.test.woe2), breaks = k, labels = FALSE)
+folds <- cut(1:nrow(testset.4), breaks = k, labels = FALSE)
 set.seed(123)
 folds <- sample(folds)
 cv_results5 <- matrix(nrow = 1, ncol = k)
 for (j in 1:k) {
   idx_val <- which(folds == j, arr.ind = TRUE)
-  cv_train <- nn.test.woe2[-idx_val,]
-  cv_val <- nn.test.woe2[idx_val,]
-  cv_results5[1,j] <- abcdefg5(nn, test, item_price, xxx)
+  cv_train <- testset.4[-idx_val,]
+  cv_results5[1,j] <- abcdefg5(nn, cv_train, price, xxx)
 }
 opt.cross.val.th5 <- mean(cv_results5)
 
@@ -274,3 +283,4 @@ theoretical.th <- round(c(exp.costs1,exp.costs3,exp.costs5,exp.costs7,exp.costs9
 empirical.th <- round(c(exp.costs.opt1,exp.costs.opt2,exp.costs.opt3,exp.costs.opt4,exp.costs.opt5))
 naming <- c("rf.cat", "rf", "lr", "lr.cat" , "nn")
 cost.matrix <- data.frame(naive.th,theoretical.th, empirical.th, row.names = naming)
+cost.matrix
